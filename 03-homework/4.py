783af3d7ef0c7d4311ee24aee556030e71022588
Myznikova Nastya <myznikova.nastya@gmail.com>
2022-02-17 19:44:49 +0300

    Adding new files

--- 03-homework/4.py
+++
@@ -1,7 +0,0 @@
-price1 = int(input('Цена первого стула: '))
-price2 = int(input('Цена второго стула: '))
-price3 = int(input('Цена третьего стула: '))
-total = price1 + price2 + price3
-if total > 10000:
-    total *= 0.1
-print('Итог:', total)
--- 03-homework/8.py
+++
@@ -1,8 +0,0 @@
-hours = int(input('Введите отработанные часы: '))
-credit = int(input('Введите остаток по кредиту: '))
-food = int(input('Введите траты на еду: '))
-salary = 200 * hours / (2 ** 3) + hours
-if salary >= credit + food:
-    print('Часов хватает. Можно отдохнуть!')
-else:
-    print('Часов не хватает. Придётся работать!')
