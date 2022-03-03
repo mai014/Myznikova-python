2198af2f795929325237d8e86734c617a307a2c9
Myznikova Nastya <myznikova.nastya@gmail.com>
2022-03-03 14:17:11 +0300

    Adding new files

--- 06-hw/10.py
+++ 06-hw/10.py
@@ -2,8 +2,8 @@ N = int(input('Введите число карточек: '))
 print('Последовательно введите', N - 1, 'оставшихся карточек: ')
 entered_list = input().split()
 num_list = list(map(int, entered_list))
-count = 0
 for i in range(1, N):
+    count = 0
     for num in num_list:
         if i != num:
             count += 1
